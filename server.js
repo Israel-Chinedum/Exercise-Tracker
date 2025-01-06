a8bd25abca02d8d55397a527dfef34c7d170ec74
Favour Israel <fastarfavour@gmail.com>
2025-01-06 12:17:35 +0000

    made more modifications to the server

--- server.js
+++ server.js
@@ -12,21 +12,41 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors());
 
-app.get('', (req, res) => {
-    res.json('connection verified');
+app.get('/api/users', (req, res) => {
+    const resData = [];
+    if(fs.existsSync('./users.json')){
+        const data = JSON.parse(fs.readFileSync('./users.json'));
+        for(let i of data){
+                resData.push({username: i.username, _id: i._id});
+        }
+        res.json(resData);
+    } else{
+        res.json(resData);
+    }
 })
 
 app.post('/api/users', (req, res) => {
     console.log(req.body);
     if(!fs.existsSync('users.json')){
-        fs.writeFile('./users.json', `[{"username": "${req.body['username']}", "_id": "${uuidv4()}"}]`, err => {
+        const data = [{
+            username: req.body['username'],
+            count: 0,
+            _id: uuidv4(),
+            log: []
+        }]
+        fs.writeFile('./users.json', JSON.stringify(data), err => {
             if(err) throw err;
         });
     } else{
         try{
             const data = fs.readFileSync('./users.json');
             const jsonData = JSON.parse(data);
-            jsonData.push({username: req.body['username'], _id: uuidv4()});
+            jsonData.push( {
+                username: req.body['username'],
+                count: 0,
+                _id: uuidv4(),
+                log: []
+            });
             fs.writeFile('./users.json', JSON.stringify(jsonData), err =>{
                 if(err) throw err;
             })
